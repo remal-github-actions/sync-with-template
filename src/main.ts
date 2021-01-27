a8ad67c1608fe30694f90f74ba85d5de0893de17
Semyon Levin <levin.semen@gmail.com>
2021-01-27 15:57:29 +0300

    draft

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
 import * as core from '@actions/core'
+import {newOctokitInstance} from "./internal/octokit"
+import {context} from "@actions/github"
 
 async function run(): Promise<void> {
     try {
-        // TODO
+        const pushToken: string = core.getInput('pushToken', {required: true})
+        const octokit = newOctokitInstance(pushToken)
+        const repo = await octokit.repos.get({
+            owner: context.repo.owner,
+            repo: context.repo.repo
+        })
+        core.warning(JSON.stringify(repo))
     } catch (error) {
-        core.setFailed(error.message)
+        core.setFailed(error)
     }
 }
 
