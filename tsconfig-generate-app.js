98c04953b340533b3d2eaa288d2cd13232c9a604
Semyon Levin <231188+remal@users.noreply.github.com>
2021-02-17 17:23:52 +0300

    Merge template repository changes: remal-github-actions/template-typescript (#42)

---
+++ tsconfig-generate-app.js
@@ -0,0 +1,17 @@
+const fs = require('fs')
+
+const encoding = 'utf8'
+const content = fs.readFileSync('tsconfig.json', encoding)
+const json = JSON.parse(content)
+fs.writeFileSync('tsconfig.json', JSON.stringify(json, null, 2) + '\n', encoding)
+
+json.compilerOptions = json.compilerOptions || {}
+json.compilerOptions.types = json.compilerOptions.types || []
+const jestTypeIndex = json.compilerOptions.types.findIndex(type => type.toLowerCase() === 'jest')
+json.compilerOptions.types.splice(jestTypeIndex, 1)
+
+json.exclude = json.exclude || []
+json.exclude.push('**/*.spec.*')
+
+const appContent = JSON.stringify(json, null, 2) + '\n'
+fs.writeFileSync('tsconfig.app.json', appContent, encoding)
