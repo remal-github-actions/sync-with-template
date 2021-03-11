041b3c426686e8e75f510187589f6a6df82c2540
remal <remal@users.noreply.github.com>
2021-03-11 11:48:01 +0000

    [push-back] Push-back updated files during build

--- dist/index.js
+++ dist/index.js
@@ -179,6 +179,7 @@ async function run() {
             return picomatch_1.default(patterns, { windows: is_windows_1.default() });
         })();
         const workspacePath = __nccwpck_require__(8517).dirSync().name;
+        __nccwpck_require__(8231).enable('simple-git');
         if (((_a = process.env.ACTIONS_STEP_DEBUG) === null || _a === void 0 ? void 0 : _a.toLowerCase()) === 'true') {
             __nccwpck_require__(8231).enable('simple-git');
         }
