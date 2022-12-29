f3dad1f1c08bc092b0f814c5be0ed6cd8d342851
remal <remal@users.noreply.github.com>
2022-12-29 17:38:13 +0000

    [push-back] Push-back updated files during build

--- dist/index.js
+++ dist/index.js
@@ -422,7 +422,7 @@ async function run() {
                                 eval: false,
                                 wasm: false,
                             });
-                            const script = `module.export = (function(){ ${transformation.script} })()`;
+                            const script = `(function(){ ${transformation.script} })()`;
                             const transformedContent = vm.run(script);
                             if (transformedContent !== content) {
                                 fs.writeFileSync(fileToSyncPath, transformedContent, 'utf8');
