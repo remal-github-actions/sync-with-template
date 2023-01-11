2ffa5184fcc1452cd1558c46b623e4c14abdddf5
Semyon Levin <levin.semen@gmail.com>
2023-01-11 13:49:06 -0800

    logging

--- src/main.ts
+++ src/main.ts
@@ -491,7 +491,7 @@ async function run(): Promise<void> {
 
                     core.info(`Pull request for '${syncBranchName}' branch has been created: ${newPullRequest.html_url}`)
                 } else {
-                    core.info(`Pull request for '${syncBranchName}' branch has been created: ${openedPr.html_url}`)
+                    core.info(`Pull request for '${syncBranchName}' branch has been updated: ${openedPr.html_url}`)
                 }
             })
 
