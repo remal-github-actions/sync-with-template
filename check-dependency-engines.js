c1e1992875057bd3286d4ce0e17a2c2daf887d58
Semyon Levin <231188+remal@users.noreply.github.com>
2023-06-09 20:06:28 -0700

    Template repository changes: remal-github-actions/template-typescript (#756)
    Co-authored-by: remal-github-actions <77626445+remal-github-actions+sync-with-template@users.noreply.github.com>

--- check-dependency-engines.js
+++ check-dependency-engines.js
@@ -2,6 +2,7 @@ const fs = require('fs')
 const semver = require('semver')
 
 let nodeVersion = fs.readFileSync('.nvmrc', 'utf8').trim()
+console.log(`Checking that engines.node in package.json file of all dependencies satisfies Node.js version ${nodeVersion}...`)
 if (nodeVersion.match(/^v?\d+$/)) nodeVersion += '.9999.9999'
 
 function processPackageJsonFiles(dir) {
