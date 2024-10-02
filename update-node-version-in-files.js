a8bff4faf48ab6c73fdae7a1e19e14a073c53f08
Semyon Levin <231188+remal@users.noreply.github.com>
2024-10-01 17:21:25 -0700

    Template repository changes: remal-github-actions/template-typescript (#1218)
    Co-authored-by: remal-github-actions <77626445+remal-github-actions+sync-with-template@users.noreply.github.com>

--- update-node-version-in-files.js
+++ update-node-version-in-files.js
@@ -77,8 +77,8 @@ function writeJsonFile(path, json) {
 ;(function () {
     const content = fs.readFileSync('.github/renovate.json5', encoding)
     const modifiedContent = content.replace(
-        /([ ]*)\/\/\s*\$\$\$sync-with-template-modifiable:\s*constraints\s*\$\$\$[\s\S]*?\/\/\s*\$\$\$sync-with-template-modifiable-end\s*\$\$\$/,
-        `$1// $$$$$sync-with-template-modifiable: constraints $$$$$\n$1force: {\n$1$1constraints: {\n$1$1$1node: "^${nodeVersion}.9999.9999",\n$1$1},\n$1},\n$1// $$$$$sync-with-template-modifiable-end$$$$$`
+        /([ ]*)\/\/\s*\$\$\$sync-with-template-modifiable:\s*(force: \{\s*)?constraints\s*\$\$\$[\s\S]*?\/\/\s*\$\$\$sync-with-template-modifiable-end\s*\$\$\$/,
+        `$1// $$$$$sync-with-template-modifiable: constraints $$$$$\n$1constraints: {\n$1$1node: "^${nodeVersion}.9999.9999",\n$1},\n$1force: {\n$1$1constraints: {\n$1$1$1node: "^${nodeVersion}.9999.9999",\n$1$1},\n$1},\n$1// $$$$$sync-with-template-modifiable-end$$$$$`
     )
     if (modifiedContent !== content) {
         fs.writeFileSync('.github/renovate.json5', modifiedContent, encoding)
